43940c0110b38dc836a22ea1f92f5f2bb4943eba
Mohamed Fawzan <fawzanm@gmail.com>
2020-05-29 21:47:27 +0400

    Emergency Updates

--- laravel.sh
+++ laravel.sh
@@ -34,7 +34,7 @@ if [ "$SHOULD_DELETE" == "1" ] ;then
     exit
 fi
 
-
+PROJECT_LOC="$PROJECT_PATH/$PROJECT_NAME"
 echo "👉 Project Location -> $PROJECT_PATH"
 echo "🚀 Creating Project -> $PROJECT_NAME "
 
@@ -52,30 +52,32 @@ fi
 
 echo "🙂 Installing the latest version of Laravel -> $PROJECT_NAME "
 cd $PROJECT_PATH && laravel new $PROJECT_NAME
-cd $PROJECT_PATH && composer require laravel-frontend-presets/tailwindcss --dev
-cd $PROJECT_PATH && php artisan ui tailwindcss --auth
+cd $PROJECT_LOC && composer require laravel-frontend-presets/tailwindcss --dev
+cd $PROJECT_LOC && php artisan ui tailwindcss --auth
 
 echo "🔭 Installing Telescope"
-cd $PROJECT_PATH && composer require laravel/telescope --dev
+cd $PROJECT_LOC && composer require laravel/telescope --dev
 
 echo "🔐 Installing Sanctum"
-cd $PROJECT_PATH && composer require laravel/sanctum
-cd $PROJECT_PATH && php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"
-cd $PROJECT_PATH && php artisan migrate
+cd $PROJECT_LOC && composer require laravel/sanctum
+cd $PROJECT_LOC && php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"
+cd $PROJECT_LOC && php artisan migrate
 
 
 echo "😎 Installing Livewire"
-cd $PROJECT_PATH && composer require livewire/livewire
+cd $PROJECT_LOC && composer require livewire/livewire
 
 echo "🚀 Installing Deployer"
-cd $PROJECT_PATH && composer require lorisleiva/laravel-deployer --dev
+cd $PROJECT_LOC && composer require lorisleiva/laravel-deployer --dev
 
 echo "👨‍💻 Installing Debugbar & IDE Helper"
-cd $PROJECT_PATH && composer require barryvdh/laravel-debugbar --dev
-cd $PROJECT_PATH && composer require barryvdh/laravel-ide-helper --devs
+cd $PROJECT_LOC && composer require barryvdh/laravel-debugbar --dev
+cd $PROJECT_LOC && composer require barryvdh/laravel-ide-helper --dev
 
 echo "📦 Installing Spatie Packages"
-cd $PROJECT_PATH && composer require spatie/laravel-permission
-cd $PROJECT_PATH && composer require spatie/laravel-log-dumper
+cd $PROJECT_LOC && composer require spatie/laravel-permission
+cd $PROJECT_LOC && composer require spatie/laravel-log-dumper
 
-echo "Make sure to run : npm install && npm run dev"
+echo "💀 Brace yourself. Installing node packages with yarn."
+cd $PROJECT_LOC && yarn
+cd $PROJECT_LOC && yarn dev
